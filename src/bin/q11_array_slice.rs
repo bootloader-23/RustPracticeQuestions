c7225f74e084f9160ac5bbb3058b18cb7926c4a0
bootloader-23 <atharva.jha708@gmail.com>
2025-07-06 21:33:55 +0530

    06 Juy, 2025 : ArrayMethods

---
+++ src/bin/q11_array_slice.rs
@@ -0,0 +1,36 @@
+fn main() {
+    let arr:[i32;5] = [1, 2, 3, 4, 5];
+    println!("arr = {:?}", arr);
+    
+    //slicing an array - sometimes some functions may require sliced arrays
+    let arr_sliced = arr.as_slice();
+    println!("arr_sliced = {:?}", arr_sliced);
+    
+    //finding the length of an array
+    let len =  arr.len();
+    println!("len = {}", len);
+    
+    //checking whether a give value is present in an array or not.
+    let ans = arr.contains(&3);
+    println!("ans = {}", ans);
+    
+    //checking in which value is at a  given index in an array.
+    let ind = 3;
+    println!("ind = {:?}", arr[ind]); // manual method
+    
+    let  ind = arr.get(1);//using arrayName.get(index)
+    println!("ind = {:?}", ind); // always use {:?} in placeholders for array and string methods
+    
+    //checking what is the value at a given index of an array
+    println!("Value at the 4th index = {}", arr[4]);
+    
+    //making a new array assigning values from old arrays
+    let x = [arr[2], arr[3]];
+    println!("x = {:?}", x);
+    // array_name[index] can be uses simply like any other variable in RUST
+    // and most other languages.
+        
+    
+   
+}
+
